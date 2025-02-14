5d8e22784dff5b3ef1e6faf7a168421ff92faa96
juan <juan_dcastro16@gmail.com>
2025-02-13 21:55:31 -0500

    base de datos que sirve con el sistema

--- (Ventas)_2025.sql
+++ (Ventas)_2025.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 11-02-2025 a las 17:13:35
+-- Tiempo de generación: 14-02-2025 a las 02:53:49
 -- Versión del servidor: 8.0.30
 -- Versión de PHP: 8.1.10
 
@@ -29,8 +29,15 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `clientes` (
   `id` int NOT NULL,
-  `id_cliente` int NOT NULL
+  `id_persona` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id`, `id_persona`) VALUES
+(4, 1);
 
 -- --------------------------------------------------------
 
@@ -39,19 +46,27 @@ CREATE TABLE `clientes` (
 --
 
 CREATE TABLE `personas` (
-  `id_cliente` int NOT NULL,
+  `id` int NOT NULL,
   `pri_nombre` varchar(35) NOT NULL,
   `seg_nombre` varchar(35) NOT NULL,
   `pri_apellido` varchar(35) NOT NULL,
   `seg_apellido` varchar(35) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `documento` int DEFAULT NULL,
-  `telefono` int DEFAULT NULL,
+  `telefono` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `correo_electronico` varchar(255) DEFAULT NULL,
   `direccion_residencia` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   `fecha_registro` date DEFAULT NULL,
   `fecha_nacimiento` date DEFAULT NULL,
   `genero` enum('F','M') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `personas`
+--
+
+INSERT INTO `personas` (`id`, `pri_nombre`, `seg_nombre`, `pri_apellido`, `seg_apellido`, `documento`, `telefono`, `correo_electronico`, `direccion_residencia`, `fecha_registro`, `fecha_nacimiento`, `genero`) VALUES
+(1, 'pepe', 'pepe', 'pepe', 'pepe', 1234567895, '1236324578', 'pepe', 'pepe', '2025-02-12', '2005-06-08', 'M'),
+(2, 'ñero', '', 'perez', '', 1234567899, '1236354785', 'ñero1345', 'la paz', '2025-02-12', '2005-06-08', 'M');
 
 --
 -- Índices para tablas volcadas
@@ -62,13 +77,13 @@ CREATE TABLE `personas` (
 --
 ALTER TABLE `clientes`
   ADD PRIMARY KEY (`id`) USING BTREE,
-  ADD KEY `FK_personas_personas` (`id_cliente`) USING BTREE;
+  ADD KEY `FK_personas_personas` (`id_persona`) USING BTREE;
 
 --
 -- Indices de la tabla `personas`
 --
 ALTER TABLE `personas`
-  ADD PRIMARY KEY (`id_cliente`),
+  ADD PRIMARY KEY (`id`) USING BTREE,
   ADD UNIQUE KEY `correo_electronico` (`correo_electronico`),
   ADD UNIQUE KEY `telefono` (`telefono`),
   ADD UNIQUE KEY `documento` (`documento`);
@@ -81,13 +96,13 @@ ALTER TABLE `personas`
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `personas`
 --
 ALTER TABLE `personas`
-  MODIFY `id_cliente` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
